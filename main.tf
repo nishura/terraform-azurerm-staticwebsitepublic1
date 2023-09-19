a002dad0ffa0369ec2361139c50982af4206f5c3
nishu rana <rana.nishu@gmail.com>
2023-09-19 08:03:50 +0530

    2.0.0 Commit

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 # Create Azure Storage account
-resource "azurerm_storage_account" "storage_account" {
+resource "azurerm_storage_account2" "storage_account" {
   name                = "${var.storage_account_name}${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.resource_group.name
  
